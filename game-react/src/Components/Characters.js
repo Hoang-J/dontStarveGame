5f0eeedfbe9a2086b62ea4cfd043a79a64e06292
COR Coding <corcoding@Coding-16.local>
2024-05-28 19:32:54 -0400

    added payment section and pulling from API

--- game-react/src/Components/Characters.js
+++ game-react/src/Components/Characters.js
@@ -1,24 +1,52 @@
 import React, { useState, useEffect } from 'react'
+import './Characters.css'
 
 function Characters() {
 
-  const [charList, setCharList] = useState('')
+  const [charList, setCharList] = useState([])
+  const listItems = charList.map(char => 
+    <ul key={char.id}>
+      <img
+      src={char.portrait}
+      />
+      <p>
+        <b>{char.nickname}</b>
+        <p>{char.name}</p>
+      </p>
+    </ul>
+  // <>
+
+    // {/* <div className='char-div'> {char.nickname}: {char.name} <img className="image" src={char.portrait}></img></div> */}
+    
+  // {/* </> */}
+  
+
+  )
+  
+
 
   useEffect(() => {
     fetch('https://dont-starve-together-api.xyz/api/survivors')
     .then(response => (response.json()))
     .then(data => {
+      // list = data
       setCharList(data)
+      // console.log(list)
     })
     
   }, [])
 
-
+  
+// console.log("outside of effect", list)
+// charCard.set('name', charList[0].name)
+// console.log(charCard.get('name'))
 
   return (
     <>
       <div>Characters</div>
-      {/* <div>{charList}</div> */}
+      <div className="test">
+      {listItems}
+      </div>
     </>
     
   )
--- game-react/src/Components/Shop.js
+++ game-react/src/Components/Shop.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Shop.css"
 
 function Shop() {
   return (
-    <div>Shop</div>
+    <>
+      {/* Wilson */}
+      <stripe-buy-button
+      buy-button-id="buy_btn_1PLUXJ2NegYeTfbF2PvX5YC3"
+      publishable-key="pk_test_51PLFek2NegYeTfbF80e5GERAmZOxeJjguiGpTeGsdFs2lcY4VjqGGXcdnXpiTtdx4MitNuZjq7JkYNlrHyZcEMpP00QdrBFr9X"
+      >
+      </stripe-buy-button>
+      {/* Wendy */}
+      <stripe-buy-button
+      buy-button-id="buy_btn_1PLTrd2NegYeTfbFN24QYfv6"
+      publishable-key="pk_test_51PLFek2NegYeTfbF80e5GERAmZOxeJjguiGpTeGsdFs2lcY4VjqGGXcdnXpiTtdx4MitNuZjq7JkYNlrHyZcEMpP00QdrBFr9X"
+      >
+      </stripe-buy-button>
+    </>
+    
   )
 }
 
