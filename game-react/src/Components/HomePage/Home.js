6168f213dfda06a7890335c2d4b2e27a83be2078
Hoang <115591486+Hoang-J@users.noreply.github.com>
2024-05-30 21:42:56 -0400

    added a border and animation to home page

--- game-react/src/Components/HomePage/Home.js
+++ game-react/src/Components/HomePage/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './Home.css'
+import chester from '../../Images/chester-run.gif'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className="main">
+        <h2 className="home-title">Don't Starve</h2>
+        <p className="home-description"></p>
+        <img className="chester" src={chester}></img>
+      </div>
+    </>
+    
+    
   )
 }
 
